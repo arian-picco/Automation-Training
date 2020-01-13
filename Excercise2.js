f3942a122b7c9279d8c35ca2689a8360443b8098
arian-picco <apicco@sparkdigital.com>
2020-01-13 14:28:43 -0300

    test

---
+++ Excercise2.js
@@ -0,0 +1,12 @@
+
+function addAndPrintNumbers(int1){
+    arrayNumbers = [];
+    while(int1<=5){
+        arrayNumbers.push(int1);
+        int1++;
+    }
+    return arrayNumbers;
+}
+
+
+console.log(addAndPrintNumbers(0));
